af560387cdf04718fb4dad400f1e4615f20a5b04
Franco <frangarcia93@icloud.com>
2023-03-29 21:27:58 -0300

    Se comenta el footer

--- front/my-app/src/components/footer/Footer.jsx
+++ front/my-app/src/components/footer/Footer.jsx
@@ -45,23 +45,23 @@ const Footer = () => {
                 />
                 <Box textAlign={{ xs: 'center', sm: 'center', fontSize: 15, }}>
 
-                    <a href="https://www.google.com.ar/maps/place/Terminal+De+%C3%93mnibus+de+Mendoza/@-32.8956179,-68.8314464,17z/data=!4m12!1m6!3m5!1s0x967e093c495d1ce3:0x7798543506fb42c6!2sTerminal+De+%C3%93mnibus+de+Mendoza!8m2!3d-32.8955869!4d-68.8302405!3m4!1s0x967e093c495d1ce3:0x7798543506fb42c6!8m2!3d-32.8955869!4d-68.8302405" ><Typography>¿Cómo llegar?</Typography></a>
+                    {/* <a href="https://www.google.com.ar/maps/place/Terminal+De+%C3%93mnibus+de+Mendoza/@-32.8956179,-68.8314464,17z/data=!4m12!1m6!3m5!1s0x967e093c495d1ce3:0x7798543506fb42c6!2sTerminal+De+%C3%93mnibus+de+Mendoza!8m2!3d-32.8955869!4d-68.8302405!3m4!1s0x967e093c495d1ce3:0x7798543506fb42c6!8m2!3d-32.8955869!4d-68.8302405" ><Typography>¿Cómo llegar?</Typography></a>
                     <a href="https://www.terminalmendoza.com.ar/institucional.html"><Typography textAlign="center">Institutcional</Typography></a>
-                    <a href="https://www.terminalmendoza.com.ar/contacto.html"><Typography textAlign="center">Contacto</Typography></a>
+                    <a href="https://www.terminalmendoza.com.ar/contacto.html"><Typography textAlign="center">Contacto</Typography></a> */}
 
                 </Box>
                 <Box textAlign={{ xs: 'center', sm: 'center', fontSize: 15 }}>
 
-                    <a href="https://www.terminalmendoza.com.ar/agencias.html" ><Typography>Agencias</Typography></a>
+                    {/* <a href="https://www.terminalmendoza.com.ar/agencias.html" ><Typography>Agencias</Typography></a>
                     <a href="https://www.terminalmendoza.com.ar/locales.html"><Typography textAlign="center">Locales comerciales</Typography></a>
-                    <a href="https://www.terminalmendoza.com.ar/#servicios"><Typography textAlign="center">Servicios</Typography></a>
+                    <a href="https://www.terminalmendoza.com.ar/#servicios"><Typography textAlign="center">Servicios</Typography></a> */}
                     {/* <a href="https://shop.terminalmendoza.com.ar/search"><Typography textAlign="center">Venta de pasajes online</Typography></a> */}
 
                 </Box>
                 <Box textAlign={{ xs: 'center', sm: 'center' }} py={4}>
-                    <a href="https://www.facebook.com/terminalmendoza"><FacebookIcon color='facebook' sx={{ fontSize: 40, marginLeft: 2, }} /> </a>
+                    {/* <a href="https://www.facebook.com/terminalmendoza"><FacebookIcon color='facebook' sx={{ fontSize: 40, marginLeft: 2, }} /> </a>
                     <a href="https://www.instagram.com/terminalmendoza/"><InstagramIcon color='instagram' sx={{ fontSize: 40, marginLeft: 2, }} /></a>
-                    <a href="https://twitter.com/terminaltm"><TwitterIcon color='twitter' sx={{ fontSize: 40, marginLeft: 2, }} /></a>
+                    <a href="https://twitter.com/terminaltm"><TwitterIcon color='twitter' sx={{ fontSize: 40, marginLeft: 2, }} /></a> */}
 
                 </Box>
                 <Box py={{xs:0, sm:4}}>
